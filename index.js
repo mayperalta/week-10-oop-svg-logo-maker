1246d898334c3c1409880e676416f5470ccf727e
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-06-04 01:03:12 -0400

    Revised JS - shapes.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const inquirer = require('inquirer');
 const fs = require ("fs");
 
 // import classes 
-const Triangle = require('./lib/shapes.js');
-const Circle = require('./lib/shapes.js');
-const Square = require('./lib/shapes.js');
+const Triangle = require('./lib/shapes');
+const Circle = require('./lib/shapes');
+const Square = require('./lib/shapes');
 
 // Create an array of questions for user input 
 const questions = [
@@ -32,14 +32,25 @@ const questions = [
     }
 ];
 
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    let svgDetails = '';
+    svgDetails = '<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg" >';
+    // tag places <text> on top of shape
+    svgDetails += '<g>';
+
+    fs.writeFile(fileName, data, error => error ? console.log(error):console.log("Success!"));
+};
+
+
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
     .then (function (response) {
       console.log(response); 
+      writeTofile("logo.svg",  'ADD FUNCTION  HERE')
       
     })
 }
 
-// Function call to initialize app
 init();
