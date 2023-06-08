de5e6f0bffdf5aae3e22774736a4431fe4563242
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-06-07 22:37:23 -0400

    Added test scripts to shapes.test.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require ("fs");
-const fileName = "./examples/logo.svg";
 const setShape = require ('./lib/setshapes'); 
 const questions = require ('./lib/questions');
 
 // function to create svq file
 function createLogo (response) {
-  const svg = setShape(response); 
+  const svg = setShape(response);
+  const fileName = './examples/' + response.shape.toLowerCase() + '.sgv'; 
+  console.log("fileName: " + fileName)
   fs.writeFile(fileName, svg, error => error ? console.log(error):console.log("Success!"));
 }
 
@@ -17,11 +18,11 @@ function init() {
     .prompt(questions)
     .then ((response) => {
       // test user input
-      console.log(response.shape)
-      console.log(response.text)
-      console.log(response.textColor)
-      console.log(response.shapeColor)
-
+      console.log("shape: " + response.shape)
+      console.log("text: " + response.text)
+      console.log("textColor: " + response.textColor)
+      console.log("shapeColor: " + response.shapeColor)
+      
       createLogo(response); 
     })
   .catch (err => {
